b05e136d2f3af3e4d3cc9061d1c3b4aa7e92375d
Steven Staubach <SteveStaubach@gmail.com>
2019-04-10 23:09:11 -0600

    Update MASTERCODE-32bitconverter.py

--- MASTERCODE-32bitconverter.py
+++ MASTERCODE-32bitconverter.py
@@ -44,6 +44,11 @@ def complexmovement():
             display.scroll(bitstring, delay=100)
         else:
             defaultmovement()
+    elif screen == 2:
+        if y < 4:
+            y = y+1
+        else:
+            y = 0
     active = False
 while True:
     if screen <= 1:
@@ -62,3 +67,19 @@ while True:
                     complexmovement()
                 while screen == 0:
                     complexmovement()
+    else:
+        if y == 0:
+            display.show(Image('05105:05105:05105:05105:00550'))
+        if y == 1:
+            display.show(Image('05555:00510:00510:00510:05555'))
+        if y == 2:
+            display.show(Image('05555:05100:05550:05100:05100'))
+        if y == 3:
+            display.show(Image('05550:05105:05100:05105:05550'))
+        if y == 4:
+            display.show(Image('05550:05105:05550:05150:05105'))
+        if button_b.is_pressed():
+            while True:
+                if not button_b.is_pressed():
+                    break
+            complexmovement()
